c8bb2ac1239f58181046fb4a587c69610bc2d4dd
Alex Lementuev <a.lementuev@gmail.com>
2015-10-26 01:48:12 -0700

    Progress

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LunarCore;
+
+public class Brick : BaseBehaviour2D
+{
+    private static readonly int[] JUMP_OFFSETS = {
+        1, 3, 5, 6, 6, 7, 7, 7, 6, 5, 4, 3, 1, -1
+    };
+
+    [SerializeField]
+    private Mushroom m_Mushroom;
+
+    protected override void OnStart()
+    {
+        Assert.IsNotNull(m_Mushroom);
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.position.y < transform.position.y)
+        {
+            Hit();
+        }
+    }
+
+    void Hit()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        StartCoroutine(JumpRoutine());
+    }
+
+    void JumpFinished()
+    {
+        Mushroom mushroom = Instantiate(m_Mushroom) as Mushroom;
+        mushroom.transform.parent = transform;
+        mushroom.transform.localPosition = Vector3.zero;
+    }
+
+    IEnumerator JumpRoutine()
+    {
+        float x = position2D.x;
+        float y = position2D.y;
+        for (int i = 0; i < JUMP_OFFSETS.Length; ++i)
+        {
+            position2D = new Vector2(x, y + 0.4f * JUMP_OFFSETS[i]);
+            yield return null;
+        }
+
+        position2D = new Vector2(x, y);
+
+        JumpFinished();
+    }
+}
---
+++ Assets/Scripts/Mushroom.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LunarCore;
+
+public class Mushroom : BaseBehaviour2D
+{
+    private Rigidbody2D m_RigidBody;
+
+    protected override void OnStart()
+    {
+        m_RigidBody = GetRequiredComponent<Rigidbody2D>();
+        m_RigidBody.isKinematic = true;
+
+        StartCoroutine(Grow());
+    }
+        
+    IEnumerator Grow()
+    {
+        yield return new WaitForSeconds(0.05f);
+
+        float targetX = position2D.x;
+        float targetY = position2D.y + 6.4f;
+        position2D = new Vector2(position2D.x, position2D.y + 0.4f * 4);
+
+        while (position2D.y < targetY)
+        {
+            yield return new WaitForSeconds(0.067f);
+            position2D = new Vector2(targetX, position2D.y + 0.4f);
+        }
+
+        position2D = new Vector2(targetX, targetY);
+
+        GrowFinished();
+    }
+
+    void GrowFinished()
+    {
+        m_RigidBody.isKinematic = false;
+        m_RigidBody.velocity = new Vector2(36, 0);
+    }
+}
