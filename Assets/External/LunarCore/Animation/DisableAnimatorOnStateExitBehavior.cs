ac24ff1e6284039db2c63dcd6cdf3f751324d5df
Alex Lementuev <a.lementuev@gmail.com>
2015-11-04 23:51:32 -0800

    Progress

---
+++ Assets/External/LunarCore/Animation/DisableAnimatorOnStateExitBehavior.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableAnimatorOnStateExitBehavior : StateMachineBehaviour
+{
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.enabled = false;
+    }
+}
