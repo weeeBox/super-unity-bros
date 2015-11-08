5d5897b199d02a0b93ba6d3a9fde89999e0237ce
Alex Lementuev <a.lementuev@gmail.com>
2015-11-07 17:02:31 -0800

    Progress

---
+++ Assets/Scripts/BillBlaster.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LunarCore;
+
+public class BillBlaster : BaseBehaviour
+{
+    [SerializeField]
+    BulletBill m_BulletBill;
+
+    [SerializeField]
+    float m_ShootingTimeMin = 3;
+
+    [SerializeField]
+    float m_ShootingTimeMax = 10;
+
+    protected override void OnStart()
+    {
+        assert.IsNotNull(m_BulletBill);
+        StartCoroutine(ShootingLoop());
+    }
+
+    IEnumerator ShootingLoop()
+    {
+        while (true)
+        {
+            float time = Random.Range(m_ShootingTimeMin, m_ShootingTimeMax);
+            yield return new WaitForSeconds(time);
+
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        BulletBill bulletBill = Instantiate(m_BulletBill) as BulletBill;
+        bulletBill.transform.parent = transform;
+        bulletBill.transform.localPosition = Vector3.zero;
+        bulletBill.direction = -1; // FIXME: check mario position
+    }
+}
---
+++ Assets/Scripts/BulletBill.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LunarCore;
+
+public class BulletBill : BaseBehaviour2D
+{
+    [SerializeField]
+    float m_Speed = 36f;
+
+    int m_Direction;
+
+    protected override void OnFixedUpdate(float deltaTime)
+    {
+        transform.Translate(m_Direction * m_Speed * deltaTime, 0f);
+    }
+
+    #region Properties
+
+    public int direction
+    {
+        get { return m_Direction; }
+        set
+        {
+            assert.IsTrue(value == 1 || value == -1);
+
+            m_Direction = value;
+            flipX = value == -1;
+        }
+    }
+
+    #endregion
+}
