4b49ac82a48d634814e37b61eef6db5a5d284d20
Alex Lementuev <a.lementuev@gmail.com>
2015-11-09 15:55:20 -0800

    Progress

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleMenu : MonoBehaviour
+{
+    #region Button Handlers
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    public void StartTest()
+    {
+        // TODO
+    }
+
+    #endregion
+}
