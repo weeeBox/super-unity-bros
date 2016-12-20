8f304bd634bb99637bafbc700f79e58a55212cd2
Alex Lementuev <a.lementuev@gmail.com>
2016-12-19 20:40:08 -0800

    Progress

---
+++ Assets/Editor/Commands.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LunarPlugin;
+
+[CCommand("give", Values="mushroom")]
+class Cmd_give : PlayerCommand
+{
+    void Execute(string item)
+    {
+        var player = GetPlayer();
+        if (item == "mushroom")
+        {
+            player.AdvanceState();
+        }
+    }
+}
+
+class PlayerCommand : CCommand
+{
+    protected PlayerController GetPlayer()
+    {
+        return GameObject.FindObjectOfType<PlayerController>();
+    }
+}
