7d53444000a8a0bb3c3b97e3c752d791670700ab
Alex Lementuev <a.lementuev@gmail.com>
2016-12-23 12:42:54 -0800

    Progress

--- Assets/Scripts/CVars.cs
+++ Assets/Scripts/CVars.cs
@@ -6,4 +6,14 @@ using LunarPlugin;
 [CVarContainer]
 public static class CVars
 {
+    public static CVar g_playerWalkSpeed        = new CVar("g_playerWalkSpeed", 48.0f);
+    public static CVar g_playerRunSpeed         = new CVar("g_playerRunSpeed", 72.0f);
+
+    public static CVar g_enemySpeed             = new CVar("g_enemySpeed", 12.0f);
+    public static CVar g_powerupSpeed           = new CVar("g_powerupSpeed", 24.0f);
+    public static CVar g_bulletBillSpeed        = new CVar("g_bulletBillSpeed", 36.0f);
+
+    public static CVar g_piranhaPlantSpeed      = new CVar("g_piranhaPlantSpeed", 12.0f);
+    public static CVar g_piranhaPlantIdleTime   = new CVar("g_piranhaPlantIdleTime", 1.0f);
+    public static CVar g_piranhaPlantHiddenTime = new CVar("g_piranhaPlantHiddenTime", 1.25f);
 }
--- Assets/Scripts/Game/Characters/Enemies/PiranhaPlantController.cs
+++ Assets/Scripts/Game/Characters/Enemies/PiranhaPlantController.cs
@@ -5,21 +5,14 @@ using LunarCore;
 
 public class PiranhaPlantController : EnemyController
 {
-    [SerializeField]
-    float m_Speed = 12f;
+    float m_height;
 
-    [SerializeField]
-    float m_IdleTime = 1f;
-
-    [SerializeField]
-    float m_HiddenTime = 1.25f;
-
-    float m_Height;
-
-    protected override void OnStart()
+    protected override void OnEnabled()
     {
-        SpriteRenderer spriteRenderer = GetRequiredComponent<SpriteRenderer>();
-        m_Height = spriteRenderer.sprite.bounds.size.y;
+        base.OnEnabled();
+
+        var spriteRenderer = GetRequiredComponent<SpriteRenderer>();
+        m_height = spriteRenderer.sprite.bounds.size.y;
         canAttack = true;
         movementEnabled = false;
         mapCollisionsEnabled = false;
@@ -29,39 +22,34 @@ public class PiranhaPlantController : EnemyController
         StartCoroutine(Loop());
     }
 
-    protected override void OnEnabled()
-    {
-        base.OnEnabled();
-    }
-
     IEnumerator Loop()
     {
         Vector3 idlePos = transform.localPosition;
-        Vector3 hiddenPos = idlePos - new Vector3(0, m_Height, 0);
+        Vector3 hiddenPos = idlePos - new Vector3(0, m_height, 0);
 
         while (true)
         {
             // Hide
             transform.localPosition = hiddenPos;
-            yield return new WaitForSeconds(m_HiddenTime);
+            yield return new WaitForSeconds(CVars.g_piranhaPlantHiddenTime.FloatValue);
 
             if (canAttack)
             {
                 // Up
                 while (posY < idlePos.y)
                 {
-                    transform.Translate(0, m_Speed * Time.fixedDeltaTime);
+                    transform.Translate(0, CVars.g_piranhaPlantSpeed.FloatValue * Time.fixedDeltaTime);
                     yield return null;
                 }
 
                 // Idle
                 transform.localPosition = idlePos;
-                yield return new WaitForSeconds(m_IdleTime);
+                yield return new WaitForSeconds(CVars.g_piranhaPlantIdleTime.FloatValue);
 
                 // Down
                 while (posY > hiddenPos.y)
                 {
-                    transform.Translate(0, -m_Speed * Time.fixedDeltaTime);
+                    transform.Translate(0, -CVars.g_piranhaPlantSpeed.FloatValue * Time.fixedDeltaTime);
                     yield return null;
                 }
             }
